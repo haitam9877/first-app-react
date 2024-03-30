15ae691c75e8c519869fc8a175bac89b5a3e9dcb
boubaouheithem <azehaitam@gmail.com>
2024-03-30 18:22:59 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 
 import './App.css';
+import Info from './Info';
+
 function App() {
   return (
     <div className="App">
+  
+      <Info name="heithem" email="haitam@gmailcom" >
+        
+        <h2>hello</h2>
+      </Info>
+      
     </div>
   );
 }
