731845e10f80a77a1290e7561b4980cd9dc9adb1
boubaouheithem <azehaitam@gmail.com>
2024-03-30 07:56:52 +0100

    back

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <div className="btn-group" role="group" aria-label="Basic example">
-        <button type="button" className="btn btn-primary">Left</button>
-        <button type="button" className="btn btn-primary">Middle</button>
-        <button type="button" className="btn btn-primary">Right</button>
-      </div>
-      <h1>Hello Main</h1>
     </div>
   );
 }
+
+
 
 export default App;
