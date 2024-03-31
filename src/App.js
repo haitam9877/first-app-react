0ef5efa4c7ed20508ff399f17ad6e1b3de4698df
boubaouheithem <azehaitam@gmail.com>
2024-03-31 10:16:40 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 
 import './App.css';
-import Info from './Info';
+
 
 function App() {
   return (
     <div className="App">
 
   
-      <Info name="heithem" email="haitam@gmailcom" >
-        
-        <h2>hello</h2>
-      </Info>
+
       <h1>heloo</h1>
 
     </div>
   );
 }
+
 
 
 
