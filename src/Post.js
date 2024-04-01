13650dea87b3d3f98c1fdbd7a837aee702c41a3c
boubaouheithem <azehaitam@gmail.com>
2024-04-01 10:44:58 +0100

    ch map lists

--- src/Post.js
+++ src/Post.js
@@ -1,30 +1,48 @@
 export default function Post() {
+
+    const posts = [
+        {
+            id: 1,
+            title: "this is post 1",
+            body: "this is body 1",
+            ch: <h2>20</h2>
+            
+        },
+        {
+            id: 2,
+            title: "this is post 2",
+            body: "this is body 2",   
+        },
+        {
+            id: 3,
+            title: "this is post 3",
+            body: "this is body 3",   
+        }
+
+    ]
+
+    const postsLists = posts.map((post) => {
+        return <PostMain key={post.id} title={post.title} body={post.body}>
+            {post.ch}
+        </PostMain>
+    })
     return (
         <div className="box-post">
-            <PostMain title="El heithem" body="This is the Post Body
-
-">
-                <h2>20</h2>
-            </PostMain>
-            <PostMain title="Hello World" body="This is the Post Body
-
-" />
-            <PostMain title="Post 3" body="This is the Post Body
-
-" />
-       
+            
+            {postsLists}
         </div>
     )
 }
-function PostMain({children,title,body}) {
 
-    //console.log(props)
-    return (
-        <div className="post">
-            {children}
-            <h2>{title}</h2>
-            <hr></hr>
-            <p>{body}</p>
-        </div>
-    )
+function PostMain({title , body , children }) {
+
+        return (
+            <div className="post">
+                {children}
+                <h2>{title}</h2>
+                <hr></hr>
+                <p>{body}</p>
+            </div>
+            )
+
 }
--- src/SideMneu.js
+++ src/SideMneu.js
@@ -1,38 +1,51 @@
 
 export default function SideMneu() {
+
+    const catigourys = [
+        {
+            id: 1,
+            btnTitle: "اكاديمية",
+            cha:   <span>🍔🍔</span>
+        },
+        {
+            id: 2,
+            btnTitle: "الاكثر قراة",
+            cha:    <div>
+                        <img style={{width: "70px"}} src="https://media1.thrillophilia.com/filestore/uwpz857lua13qmvub6um2v93dlrm_IMG%20Worlds%20%20of%20Adventure.jpg" ></img>
+                    </div>
+            
+        },
+        {
+            id: 3,
+            btnTitle: "مميز",
+            cha:<>
+                    <h1>Click Me</h1>
+                    <span>👍</span>
+                </>
+        }
+    ]
+    const catigourysLists = catigourys.map((cat) => {
+        return <Btn key={cat.id} btnTitle={cat.btnTitle}>
+            {cat.cha}
+        </Btn>
+    })
     return (
         <div className="side-mneu">
             <div className="box-btn">
-                <Btn btnTitle="اكاديمية">
-                
-                    <span>🍔🍔</span>
-                    
-                </Btn>
-
-                <Btn btnTitle="الاكثر قراة">
-                
-                    <div>
-                        <img style={{width: "70px"}} src="https://media1.thrillophilia.com/filestore/uwpz857lua13qmvub6um2v93dlrm_IMG%20Worlds%20%20of%20Adventure.jpg" ></img>
-                    </div>
-                
-                </Btn>
-                
-                <Btn btnTitle="مميز">
-                
-                    <h1>Click Me</h1>
-                    <span>👍</span>
-                
-                </Btn>
-                <Btn btnTitle="مقلاات الاسبوع"/>
-            
+                {catigourysLists}
             </div>
         </div>
     )
 }
 
 function Btn({btnTitle,children}) {
-    return <button className="btn">
-        {btnTitle}
-        {children}
-    </button>
+    return (
+        <>
+        {btnTitle == null || btnTitle === "" ? <></> : <button className="btn">
+            {btnTitle}
+            {children}
+        </button> }
+        </>
+    
+    )
 }
