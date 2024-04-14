3edf6969c36b1fa136a038582469390235ceb5c4
boubaouheithem <azehaitam@gmail.com>
2024-04-14 10:33:36 +0100

    r

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import AccordionUsage from "./ApplicitonFrist";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AccordionUsage />
+    </div>
+  );
 }
 
 export default App;
