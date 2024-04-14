dd725fc0d68e25a2d8fce8878b19d700b2138cdc
boubaouheithem <azehaitam@gmail.com>
2024-04-14 09:49:35 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import "./App.css";
-import MystatsArry from "./MystatsArry";
-import ShoppingCart from "./ShoppingCart";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <MystatsArry /> */}
-      <ShoppingCart />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
